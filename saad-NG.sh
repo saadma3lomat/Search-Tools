cf648db5739ce535f58a5e4ac84dc16227396e75
saad morocco <67868906+saadma3lomat@users.noreply.github.com>
2021-03-31 15:10:25 -0400

    Add files via upload

--- saad-NG.sh
+++ saad-NG.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
-echo "Welcome to 9ism al ma3lomat By  Saad NJ :)  "  ; sleep 2
+#!/usr/bin/bash
+echo "Welcome to 9ism Al Ma3lomat By  Saad NJ :)" ; sleep 2
 echo "Youtube  :  https://www.youtube.com/channel/UCYs_yQNKlr9P3-Gkx7v9voA" ; sleep 2
 echo "Tiktok   :  @saad_linux" ; sleep 1
 echo 'Telegram =====> https://t.me/joinchat/UKfy4AZaMAB8H_OC0' ; sleep 2
 echo "Termux updating  +++++++++++++++++++++++++" ; sleep 3
 
 pkg update ; pkg updrade -y ; sleep 2 ; pkg install wget -y ; wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh ; chmod 777 install.sh ; bash install.sh -i ; sleep 2
-echo "[i] Installing Saad-NJ-Termu-XBlacK ..."
+echo "[i] Installing Saad-NJ-Termux-BlacK ..."
 sed -i s:PS1.*:"PS1=\'\\\\[\\\\e\[1\;34m\\\\]Saad-NJ-Termux-Black > \\\[\\\e[0;37m\\\\]\'": $PREFIX/etc/bash.bashrc
 
 echo -e "
@@ -16,7 +16,7 @@ ktb =====>  bash install.sh -i
 ila bghiti tms7o :
 ktb =====>  bash install.sh -u
 
-matnsach t2abona f9anat youtube ===> Saad NJ :)
+Matnsach T2abona F9anat Youtube ===> Saad NJ :)
 https://www.youtube.com/channel/UCYs_yQNKlr9P3-Gkx7v9voA
 
 Tiktok =====> @saad_linux
@@ -53,8 +53,11 @@ echo "[i] Sorry :( "
 printf "Are you want Uninstall Saad-NJ-Termux-Black PS1? [Y/n] "
 read ask
 if [[ "$ask" == [Y/y] ]];then
+bash install.sh -u
 rm -f $PREFIX/etc/apt/sources.list.d/termuxblack.list && mv $PREFIX/etc/bash.bashrc.bk $PREFIX/etc/bash.bashrc && rm>
+clear ; rm -rif install.sh install.sh.1 install.sh.2 termuxblack.key ; ls ; cat Saad.NJ.txt
 echo "[i] The Saad-NJ-Termux-Black exiting  :("
+echo "[i] Restart termux ..."
 exit 0
 fi
 
