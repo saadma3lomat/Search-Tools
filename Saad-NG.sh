ef05b9f1eb867450fc39e1aec9c6a36c5132c508
saad morocco <67868906+saadma3lomat@users.noreply.github.com>
2021-03-30 21:13:44 -0400

    Add files via upload

---
+++ Saad-NG.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Youtube  :  https://youtu.be/3Za9oGnu0Co"
+echo "Tiktok : @saad_linux"
+echo "termux updating  +++++++++++++++++++++++++"
+pkg update ; pkg install wget ; wget https://github.com/Hax4us/TermuxBlack/blob/master/install.sh ; chmod +x install.sh ; bash install.sh -i
+sed -i s:PS1.*:"PS1=\'\\\\[\\\\e\[1\;34m\\\\]Saad-NJ-Termux-Black > \\\[\\\e[0;37m\\\\]\'": $PREFIX/etc/bash.bashrc
+pkg update ; pkg upgrade -y ; pkg clean ; clear ; bash
+echo 'ktb bash install.sh -i ila baghi tkhdmo    ila bghiti tms7o ktb bash install.sh -u         matnsach t2abona f9anat youtube = Saad NJ   https://www.youtube.com/channel/UCYs_yQNKlr9P3-Gkx7v9voA     Tiktok = Saad_linux' > Saad-NG.txt
+cat Saad-NG.txt  
